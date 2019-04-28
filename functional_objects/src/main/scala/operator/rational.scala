19c879ac77a71c2a0bd3ecaa90b3669215b8c925
KentFujii <kent.where.the.light.is@gmail.com>
2019-04-28 19:03:44 +0900

    finish: functional_objects

---
+++ functional_objects/src/main/scala/operator/rational.scala
@@ -0,0 +1,35 @@
+package operator
+
+class Rational(n: Int, d: Int) {
+  require(d != 0)
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+  def this(n: Int) = this(n, 1)
+  def + (that: Rational): Rational = {
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+  }
+  def * (that: Rational): Rational = {
+    new Rational(
+      numer * that.numer,
+      denom * that.denom
+    )
+  }
+  override def toString = numer + "/" + denom
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
+  }
+}
+
+object Rational extends App {
+  val x = new Rational(1, 2)
+  println(x)
+
+  val y = new Rational(2, 3)
+  println(y)
+
+  println(x + y)
+}
